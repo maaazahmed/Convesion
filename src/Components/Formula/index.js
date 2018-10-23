b97f0692976b0e9bd68f8f851ff388b15c4081a6
maaazahmed <maazahmed2k16@gmail.com>
2018-10-23 22:51:50 +0500

    Updateed

--- src/Components/Formula/index.js
+++ src/Components/Formula/index.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import {TouchableOpacity} from "react-native"
-import { Container, Header, Left, Body, Right, Button, Icon, Title, View } from 'native-base';
+import { TouchableOpacity, View, Text, Dimensions } from "react-native"
+import { Container, Content, Header, Icon } from 'native-base';
 
 
 
-
+const { width, height } = Dimensions.get("window")
 export default class Formulas extends Component {
+
     render() {
+        let arr = []
+        for (var i = 0; i < 30; i++) {
+            arr.push(
+                <View style={{ backgroundColor: "green", height: height / 15, width, borderBottomColor: "#312e3f", borderBottomWidth: 1 }} ></View>
+            )
+        }
         return (
             <Container>
                 <Header style={{ backgroundColor: "#312e3f", alignItems: "center" }} >
-                    <View style={{flex:1, justifyContent:"center"}} >
+                    <View style={{ flex: 1, justifyContent: "center" }} >
                         <TouchableOpacity activeOpacity={0.6} >
-                            <Icon  style={{color:"#fff", fontSize:22}} name='arrow-back' />
+                            <Icon style={{ color: "#fff", fontSize: 22 }} name='arrow-back' />
                         </TouchableOpacity>
                     </View>
                 </Header>
+                <Content>
+                    {arr}
+                </Content>
+
             </Container>
         );
     }
