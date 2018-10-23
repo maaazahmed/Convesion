8917f3363048f61773283f5b21eccdb5f44d5bed
maaazahmed <maazahmed2k16@gmail.com>
2018-10-23 22:39:03 +0500

    Updateed

--- src/Components/Formula/index.js
+++ src/Components/Formula/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import {TouchableOpacity} from "react-native"
+import { Container, Header, Left, Body, Right, Button, Icon, Title, View } from 'native-base';
 
 
 
@@ -8,20 +9,12 @@ export default class Formulas extends Component {
     render() {
         return (
             <Container>
-                <Header style={{backgroundColor:"#312e3f"}} >
-                    <Left>
-                        <Button transparent>
-                            <Icon name='arrow-back' />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Formula</Title>
-                    </Body>
-                    <Right>
-                        <Button transparent>
-                            {/* <Icon name='menu' /> */}
-                        </Button>
-                    </Right>
+                <Header style={{ backgroundColor: "#312e3f", alignItems: "center" }} >
+                    <View style={{flex:1, justifyContent:"center"}} >
+                        <TouchableOpacity activeOpacity={0.6} >
+                            <Icon  style={{color:"#fff", fontSize:22}} name='arrow-back' />
+                        </TouchableOpacity>
+                    </View>
                 </Header>
             </Container>
         );
