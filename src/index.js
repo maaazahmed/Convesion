fc1554f06511f3b63c30a79ea68cbe3b731581b5
maaazahmed <maazahmed2k16@gmail.com>
2018-10-23 22:13:01 +0500

    Updateed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import { Categorry } from "./Components"
 
 const Routes = StackNavigator({
     Categorry: { screen: Categorry }
-})
+}, {
+        navigationOptions: {
+            header: null
+        }
+    })
 
 
 export default Routes
