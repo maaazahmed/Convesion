539f6bf404210d0c661447ccefdcaa896c8eb4f8
maaazahmed <maazahmed2k16@gmail.com>
2018-10-24 08:26:15 +0500

    updated

--- src/store/reducer/reducer.js
+++ src/store/reducer/reducer.js
@@ -14,90 +14,9 @@ export default (state = INITIAL_STATE, action) => {
         case ActionTypes.REQUEST_LIST:
             return ({
                 ...state,
-                requestList: action.payload
+                formulas: action.payload
             })
-        case ActionTypes.CATEGORY_LIST:
-            return ({
-                ...state,
-                categoryList: action.payload
-            })
-        case ActionTypes.CATEGORY_DATA:
-            return ({
-                ...state,
-                categoryData: action.payload
-            })
-        case ActionTypes.CATEGORY_ID:
-            return ({
-                ...state,
-                categoryID: action.payload
-            })
-        case ActionTypes.COVER_IMAGE:
-            return ({
-                ...state,
-                coverImage: action.payload
-            })
-        case ActionTypes.MY_PRODUCT:
-            return ({
-                ...state,
-                myProducts: action.payload
-            })
-        case ActionTypes.SELECTED_PRODUCTS:
-            return ({
-                ...state,
-                selectedProduct: action.payload
-            })
-        case ActionTypes.SELLER_LIST:
-            return ({
-                ...state,
-                sellerList: action.payload
-            })
-        case ActionTypes.SHOPKEEPER_ID:
-            return ({
-                ...state,
-                shopkeeperID: action.payload
-            })
-        case ActionTypes.ORDER_LIST:
-            return ({
-                ...state,
-                orderList: action.payload,
-                coverImageUrl: action.coverImageUrl
-
-            })
-        case ActionTypes.ORDER_DETAILS:
-            return ({
-                ...state,
-                orderDetails: action.payload,
-
-            })
-        case ActionTypes.ACCEPTED_ORDERS:
-            return ({
-                ...state,
-                myOrders: action.payload,
-
-            })
-        case ActionTypes.REJECTED_ORDER:
-            return ({
-                ...state,
-                reJectedOrders: action.payload,
-
-            })
-        case ActionTypes.SIGN_IN_ROUT:
-            return ({
-                ...state,
-                signInRout: action.payload,
-            })
-
-        case ActionTypes.DASHBOARD_ROUT:
-            return ({
-                ...state,
-                DashboardRout: action.payload,
-            })
-        case ActionTypes.DEVICES_TOKEN:
-            return ({
-                ...state,
-                devicesToken: action.payload,
-            })
-     
+    
         default:
             return state;
     }
