af7eafc708f2e316800b69a593c3f1c4fa742d2e
maaazahmed <maazahmed2k16@gmail.com>
2018-11-01 21:52:09 +0500

    Updated

--- src/Components/Formula/solveFormula/Volume/index.js
+++ src/Components/Formula/solveFormula/Volume/index.js
@@ -154,52 +154,59 @@ class Volume extends Component {
 
     render() {
         return (
-            <Container style={{ backgroundColor: "#373447" }} >
-                <Header style={styles.Header} >
-                    <View style={styles.heightIconCintainer} >
-                        <TouchableOpacity
-                            onPress={() => this.openDrawer()}
-                            style={{ alignSelf: "flex-start" }}
-                            activeOpacity={0.6} >
-                            <Icon style={styles.heightIcon} name='menu' />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.heightIconCintainer} >
-                        <TouchableOpacity
-                            onPress={() => this.openDrawer()}
-                            style={{ alignSelf: "flex-end" }}
-                            onPress={() => this.props.navigation.navigate("Categorry")}
-                            activeOpacity={0.6} >
-                            <Icon style={styles.heightIcon} name='home' />
-                        </TouchableOpacity>
-                    </View>
-                </Header>
-                <View style={styles.solveContainer} >
-                    <View style={styles.inputCiintainer} >
-                        <View>
-                            <TextInput
-                                value={this.state.valOne}
-                                onChangeText={(event) => this.valOneHeandler(event)}
-                                keyboardType="numeric"
-                                underlineColorAndroid="transparent"
-                                placeholderTextColor="#fff"
-                                placeholder={this.state.placeholder1}
-                                style={styles.TextInput} />
+            <Drawer
+                ref={(ref) => { this.drawer = ref; }}
+                closedDrawerOffset={0}
+                openDrawerOffset={0.3}
+                content={<DrowerCategory navigation={this.props.navigation} />}
+                onClose={() => this.closeDrawer()} >
+                <Container style={{ backgroundColor: "#373447" }} >
+                    <Header style={styles.Header} >
+                        <View style={styles.heightIconCintainer} >
+                            <TouchableOpacity
+                                onPress={() => this.openDrawer()}
+                                style={{ alignSelf: "flex-start" }}
+                                activeOpacity={0.6} >
+                                <Icon style={styles.heightIcon} name='menu' />
+                            </TouchableOpacity>
                         </View>
-                        <View style={{ marginTop: 30 }} >
-                            <TextInput
-                                onChangeText={(event) => this.valTwoHeandler(event)}
-                                value={this.state.valTwo}
-                                keyboardType="numeric"
-                                underlineColorAndroid="transparent"
-                                placeholder={this.state.placeholder2}
-                                placeholderTextColor="#fff"
-                                style={styles.TextInput} />
+                        <View style={styles.heightIconCintainer} >
+                            <TouchableOpacity
+                                onPress={() => this.openDrawer()}
+                                style={{ alignSelf: "flex-end" }}
+                                onPress={() => this.props.navigation.navigate("Categorry")}
+                                activeOpacity={0.6} >
+                                <Icon style={styles.heightIcon} name='home' />
+                            </TouchableOpacity>
                         </View>
+                    </Header>
+                    <View style={styles.solveContainer} >
+                        <View style={styles.inputCiintainer} >
+                            <View>
+                                <TextInput
+                                    value={this.state.valOne}
+                                    onChangeText={(event) => this.valOneHeandler(event)}
+                                    keyboardType="numeric"
+                                    underlineColorAndroid="transparent"
+                                    placeholderTextColor="#fff"
+                                    placeholder={this.state.placeholder1}
+                                    style={styles.TextInput} />
+                            </View>
+                            <View style={{ marginTop: 30 }} >
+                                <TextInput
+                                    onChangeText={(event) => this.valTwoHeandler(event)}
+                                    value={this.state.valTwo}
+                                    keyboardType="numeric"
+                                    underlineColorAndroid="transparent"
+                                    placeholder={this.state.placeholder2}
+                                    placeholderTextColor="#fff"
+                                    style={styles.TextInput} />
+                            </View>
 
+                        </View>
                     </View>
-                </View>
-            </Container>
+                </Container>
+            </Drawer>
         );
     }
 }
