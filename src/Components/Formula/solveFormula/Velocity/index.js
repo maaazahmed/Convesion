e0b3e2f1048fedc5b0f97d6435d735a8a5adaf95
maaazahmed <maazahmed2k16@gmail.com>
2018-11-01 22:40:20 +0500

    Updated

--- src/Components/Formula/solveFormula/Velocity/index.js
+++ src/Components/Formula/solveFormula/Velocity/index.js
@@ -231,6 +231,9 @@ const mapStateToProp = (state) => {
 };
 const mapDispatchToProp = (dispatch) => {
     return {
+        backtoSoveKey: (data) => {
+            dispatch(backtoSoveKey(data))
+        }
     };
 };
 
