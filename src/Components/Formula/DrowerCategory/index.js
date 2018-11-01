8a991ff2c6c45a85871b98348c3a5ccdec9d513b
maaazahmed <maazahmed2k16@gmail.com>
2018-11-01 18:26:19 +0500

    Updated

--- src/Components/Formula/DrowerCategory/index.js
+++ src/Components/Formula/DrowerCategory/index.js
@@ -304,6 +304,17 @@ class DrowerCategory extends Component {
                                 style={styles.categorryIcon} resizeMode="contain" />
                         </View>
                     </TouchableOpacity>
+                    <TouchableOpacity onPress={this.selectConversationn.bind(this, "Force")}
+                        activeOpacity={0.5}
+                        style={styles.categorryButn}  >
+                        <View style={styles.TextView} >
+                            <Text style={styles.categorryText} >Force</Text>
+                        </View>
+                        <View style={styles.ImageView} >
+                            <Image source={require("./images/force-meter.png")}
+                                style={styles.categorryIcon} resizeMode="contain" />
+                        </View>
+                    </TouchableOpacity>
 
 
 
